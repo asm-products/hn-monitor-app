970ab55c03b712b33ba282d5455147e743c11122
Garrett Martin <garrettqmartin8@optimum.net>
2014-10-18 18:10:02 -0500

    User rspec in generators.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module HnMonitorApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
