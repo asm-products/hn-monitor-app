3db112ee8634692acfdcd65dac1688f79b444dbe
Garrett Martin <garrettqmartin8@optimum.net>
2014-10-18 18:29:38 -0500

    Add story model and some rspec config.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016195021) do
+ActiveRecord::Schema.define(version: 20141018230558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,11 +27,15 @@ ActiveRecord::Schema.define(version: 20141016195021) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+
+  create_table "stories", force: true do |t|
+    t.string   "title"
+    t.string   "content"
+    t.string   "link"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
 end
