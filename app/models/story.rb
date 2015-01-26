d00739d7793940b5b13f4b034c21f23a391c2ecd
garrett <g.kocher@gmail.com>
2015-01-25 18:23:18 -0800

    update story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,7 @@
 class Story < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
-  validates :content, presence: true
+  validates :hn_id, presence: true
   validates :link, presence: true
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141127052937) do
+ActiveRecord::Schema.define(version: 20150125231805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "stories", force: true do |t|
     t.string   "title"
-    t.string   "content"
     t.string   "link"
-
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "hn_id"
   end
 
   create_table "triggers", force: true do |t|
@@ -58,4 +57,5 @@ ActiveRecord::Schema.define(version: 20141127052937) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["email_frequency"], name: "index_users_on_email_frequency", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
 end
