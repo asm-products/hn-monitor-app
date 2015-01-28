d3598ac26c76cf487cca4f3526d6968d205dc1f8
garrett <g.kocher@gmail.com>
2015-01-28 10:20:00 -0800

    add scopes to Trigger

--- app/models/trigger.rb
+++ app/models/trigger.rb
@@ -2,10 +2,16 @@ class Trigger < ActiveRecord::Base
   validates_presence_of :trigger_type, :word
   validate 'must_be_valid_type'
   validates_uniqueness_of :word, scope: :trigger_type
+
   has_many :user_triggers
   has_many :users, through: :user_triggers
 
   VALID_TYPES = ['domain', 'keyword', 'username']
+
+
+  scope :username, -> { where(trigger_type: 'username') }
+  scope :keyword, -> { where(trigger_type: 'keyword') }
+  scope :domain, -> { where(trigger_type: 'domain')}
 
   def must_be_valid_type
     unless VALID_TYPES.include?(self.trigger_type)
