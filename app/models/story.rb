8186fd0b873817fa02e720265ba0f9e060bce950
garrett <g.kocher@gmail.com>
2015-01-28 11:23:18 -0800

    add triggers to stories before validation

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,6 @@
 class Story < ActiveRecord::Base
+  before_validation :add_triggers
+
   validates :title, presence: true, uniqueness: true
   validates :hn_id, presence: true
   validates :link, presence: true
@@ -9,5 +11,36 @@ class Story < ActiveRecord::Base
   has_many :triggers, through: :story_triggers
 
 
+  protected
+  def add_triggers
+    check_title
+    check_user
+    check_link
+  end
+
+  def check_user
+    Trigger.usernames.each do |trig|
+      if trig.word == self.hn_user
+        self.triggers << trig
+        break
+      end
+    end
+  end
+
+  def check_title
+    Trigger.keywords.each do |trig|
+      if self.title.include?(trig.word)
+        self.triggers << trig
+      end
+    end
+  end
+
+  def check_link
+    Trigger.domains.each do |trig|
+      if self.link.include?(trig.word)
+        self.triggers << trig
+      end
+    end
+  end
 
 end
