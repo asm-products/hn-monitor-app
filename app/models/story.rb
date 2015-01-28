254084abaf3b784ba9cf7b53314ade95f13bd8d3
garrett <g.kocher@gmail.com>
2015-01-28 11:21:48 -0800

    add hn_user to story

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,7 +2,7 @@ class Story < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
   validates :hn_id, presence: true
   validates :link, presence: true
-  #validates :hn_user, presence: true
+  validates :hn_user, presence: true
   validates :triggers, presence: true
 
   has_many :story_triggers
